c92fcd20bf84c8942c3e45e5f9d809bfc60546b6
juan <martinezjuan057@gmail.com>
2023-12-18 11:27:54 -0600

    Worked up until 1.6: Arrays.

--- chapter-1/copy.c
+++ chapter-1/copy.c
@@ -54,11 +54,55 @@ void replaceSpace() {
 }
 
 /**
+ * Exercise 1-10. Write a program to copy its input to its output, replacing
+ * each tab by '\t,' each backspace by '\b,' and each backslash by '\\.'
+ * This makes tabs and backspaces visible in an unamibiguous way.
+*/
+
+void replaceSpecial() {
+    int c;
+
+    // Room for improvement: printf can make statements concise
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+            c = getchar();
+        }
+        if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+            c = getchar();
+        }
+        if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+            c = getchar();
+        }
+        putchar(c);
+    }
+}
+
+/**
+ * Exercise 1-12. Write a program that prints its input one word per line.
+*/
+
+void wordPerLine() {
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n')
+            putchar('\n');
+        else
+            putchar(c);
+    }
+}
+
+/**
  * Exercise 1-7. Write a program to print the value of EOF.
 */
 
 int main() {
     // Value of EOF is -1
     printf(" Value of EOF: %d\n", EOF);
-    replaceSpace();
+    wordPerLine();
 }
--- chapter-1/count.c
+++ chapter-1/count.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define IN 1 // Inside a word
+#define OUT 0 // Outside a word
+
 /* count characters in input; 1st version */
 
 void countChars_1(){
@@ -22,26 +25,54 @@ void countChars_2(){
 }
 
 void countLines() {
-    int c, n1;
+    int c, nl;
 
-    n1 = 0;
+    nl = 0;
     while ((c = getchar()) != EOF)
         if (c == '\n')
-            ++n1;
-    printf("%d\n", n1);
+            ++nl;
+    printf("%d\n", nl);
 }
 
 /**
  * Exercise 1-8. Write a program to count blanks, tabs, and newlines.
 */
 
-int countSpacing() {
+void countSpacing() {
     int c, n;
 
     while ((c = getchar()) != EOF)
         if (c == ' ' || c == '\t' || c == '\n')
             ++n;
     printf("%d\n", n);
+}
+
+/**
+ * Exercise 1-11. How would you test the word count program? What kinds of
+ * input are most likely to uncover bugs if there are any?
+ * 
+ * Backspaces may present an issue; may be counted as words. If reading a
+ * file as input, words that span multiple lines may be counted as two.
+ * If numbers are put into input, they may be counted as words as well.
+*/
+
+void countWords() {
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d", nl, nw, nc);
 }
 
 int main() {
