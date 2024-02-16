d023a009e7fb30ac99c8b98affc35c6546f344c3
Juan C Martinez <martinezjuan057@gmail.com>
2024-02-16 10:51:35 -0600

    2-1 output now pretty.

--- chapter-2/ranges.c
+++ chapter-2/ranges.c
@@ -12,32 +12,30 @@
 */
 int main() {
     // Integer values, use %lli for Windows, %lu for macOS
-    printf("\n   \
-            CHAR:    max = %d, min = %d\n   \
-            U_CHAR:  max = %d, min = %d\n   \
-            SHORT:   max = %d, min = %d\n   \
-            U_SHORT: max = %d, min = %d\n   \
-            INT:     max = %d, min = %d\n   \
-            U_INT:   max = %u, min = %d\n   \
-            LONG:    max = %li, min = %li\n   \
-            U_LONG:  max = %lu, min = %d\n \
-            ", CHAR_MAX, CHAR_MIN,          \
-            UCHAR_MAX, 0,                   \
-            SHRT_MAX, SHRT_MIN,             \
-            USHRT_MAX, 0,                   \
-            INT_MAX, INT_MIN,               \
-            UINT_MAX, 0,                    \
-            LONG_MAX, LONG_MIN,             \
+    printf("CHAR:    max = %d, min = %d\n"   
+            "U_CHAR:  max = %d, min = %d\n"   
+            "SHORT:   max = %d, min = %d\n"   
+            "U_SHORT: max = %d, min = %d\n"   
+            "INT:     max = %d, min = %d\n"   
+            "U_INT:   max = %u, min = %d\n"   
+            "LONG:    max = %li, min = %li\n"   
+            "U_LONG:  max = %lu, min = %d\n", 
+            CHAR_MAX, CHAR_MIN,          
+            UCHAR_MAX, 0,                   
+            SHRT_MAX, SHRT_MIN,             
+            USHRT_MAX, 0,                   
+            INT_MAX, INT_MIN,               
+            UINT_MAX, 0,                    
+            LONG_MAX, LONG_MIN,             
             ULONG_MAX, 0);
 
     // Double values
-    printf("\n   \
-            FLOAT:   max = %e, min = %e\n   \
-            DOUBLE:  max = %e, min = %e\n   \
-            L_DBL:   max = %e, min = %e\n   \
-             ", FLT_MAX, FLT_MIN,
-             DBL_MAX, DBL_MIN,
-             LDBL_MAX, LDBL_MIN);
+    printf("\nFLOAT:   max = %e, min = %e\n"   
+            "DOUBLE:  max = %e, min = %e\n"   
+            "L_DBL:   max = %e, min = %e\n",   
+            FLT_MAX, FLT_MIN,
+            DBL_MAX, DBL_MIN,
+            LDBL_MAX, LDBL_MIN);
 
     return 0;
 }
