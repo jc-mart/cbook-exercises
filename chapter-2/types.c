3b1db20b4962b67affd4dccfc3c231bcaccaaf2a
Juan C Martinez <jmartinez29@luc.edu>
2024-03-02 17:15:09 -0500

    Exercise 2-4 complete.

--- chapter-2/types.c
+++ chapter-2/types.c
@@ -3,9 +3,17 @@
 
 void expand();
 int htoi(char s[]);
+void squeeze(char s1[], char s2[]);
 
 int main() {
     printf("Result of htoi: %d\n", htoi("0X7cF")); // Decimal equivalent: 1999
+    
+    // Makes these modifiable. If hardcoded into `squeeze`, would try to write
+    // to a read-only, causing bus error.
+    char test[] = "dog";
+    char eval[] = "cat";
+    
+    squeeze(test, eval);
 }
 
 /**
@@ -64,4 +72,30 @@ int htoi(char s[]) {
     }
 
     return sum;
+}
+
+/**
+ * Write an alternate version of `squeeze(s1, s2)` that deletes each character
+ * in `s1` that matches any character in the string `s2`.
+*/
+void squeeze(char s1[], char s2[]) {
+    int i, j, k, l;
+
+    for (i = j = 0; s1[i] != '\0'; i++) {
+        // Flag to determine if a match was made
+        l = 0;
+
+        // Check current character matches any character from supplied string
+        for (k = 0; s2[k] != '\0' && !l; k++) {
+            if (s1[i] == s2[k] && !l)
+                l = 1;
+        }
+
+        // Append character if no match was made
+        if (!l)
+            s1[j++] = s1[i];
+    }
+    s1[j] = '\0';
+    // Print results
+    printf("%s\n", s1);
 }
